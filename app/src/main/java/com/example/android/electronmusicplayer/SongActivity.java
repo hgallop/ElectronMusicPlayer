d2ebba237b1d35a85fd7bbe2321cc63988245bd9
Heather Gallop <doyouseeitmyway@gmail.com>
2018-05-19 15:57:06 -0400

    Set onItemClickListener to the List of Music objects. Create intent to launch NowPlayingActivity. Send the position value to the NowPlayingActivity as an extra.

--- app/src/main/java/com/example/android/electronmusicplayer/SongActivity.java
+++ app/src/main/java/com/example/android/electronmusicplayer/SongActivity.java
@@ -3,7 +3,10 @@ package com.example.android.electronmusicplayer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -237,5 +240,15 @@ public class SongActivity extends AppCompatActivity {
         SongAdapter songAdapter = new SongAdapter(this, music);
 
         musicList.setAdapter(songAdapter);
+
+        musicList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent = new Intent(SongActivity.this, NowPlayingActivity.class);
+                intent.putExtra("pos", position);
+                startActivity(intent);
+            }
+        });
     }
 }
