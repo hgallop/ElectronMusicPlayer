282e5c228e9bf62c6e9adc3c5cad32380e9203ee
Heather Gallop <doyouseeitmyway@gmail.com>
2018-05-19 15:18:35 -0400

    Add AlbumActivity. Add onClickListener to album TextView with intent to start AlbumActivity.

---
+++ app/src/main/java/com/example/android/electronmusicplayer/AlbumActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.electronmusicplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlbumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
--- app/src/main/java/com/example/android/electronmusicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/electronmusicplayer/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.electronmusicplayer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView albums = findViewById(R.id.albums);
+        TextView songs = findViewById(R.id.songs);
+        TextView playing = findViewById(R.id.playing);
+
+        albums.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AlbumActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
