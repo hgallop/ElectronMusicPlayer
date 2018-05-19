ef49027144e156d6746f7d7bc6cf9100c72f7a2c
Heather Gallop <doyouseeitmyway@gmail.com>
2018-05-19 15:35:10 -0400

    Create ArrayList of Music objects to represent albums. Set music objects to album items with AlbumAdapter object. Set AlbumAdapter object to the ListView layout.

--- app/src/main/java/com/example/android/electronmusicplayer/AlbumActivity.java
+++ app/src/main/java/com/example/android/electronmusicplayer/AlbumActivity.java
@@ -2,6 +2,9 @@ package com.example.android.electronmusicplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class AlbumActivity extends AppCompatActivity {
 
@@ -9,6 +12,37 @@ public class AlbumActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        ArrayList<Music> music = new ArrayList<>();
 
+        music.add(new Music(R.drawable.bensound_album_cover,
+                getResources().getString(R.string.bensound_sample_hits),
+                getResources().getString(R.string.sample_description)));
+        music.add(new Music(R.drawable.generic_cover,
+                getResources().getString(R.string.generic_one),
+                getResources().getString(R.string.generic_desc)));
+        music.add(new Music(R.drawable.generic_cover_two,
+                getResources().getString(R.string.generic_two),
+                getResources().getString(R.string.generic_desc)));
+        music.add(new Music(R.drawable.generic_cover_3,
+                getResources().getString(R.string.generic_three),
+                getResources().getString(R.string.generic_desc)));
+        music.add(new Music(R.drawable.generic_cover_four,
+                getResources().getString(R.string.generic_four),
+                getResources().getString(R.string.generic_desc)));
+        music.add(new Music(R.drawable.generic_cover_five,
+                getResources().getString(R.string.generic_five),
+                getResources().getString(R.string.generic_desc)));
+        music.add(new Music(R.drawable.generic_cover_six,
+                getResources().getString(R.string.generic_six),
+                getResources().getString(R.string.generic_desc)));
+        music.add(new Music(R.drawable.generic_cover_seven,
+                getResources().getString(R.string.generic_seven),
+                getResources().getString(R.string.generic_desc)));
+
+        ListView musicList = findViewById(R.id.list);
+
+        AlbumAdapter albumAdapter = new AlbumAdapter(this, music);
+
+        musicList.setAdapter(albumAdapter);
     }
 }
