23714716809514605ff85be7341b8e92e19263d3
Heather Gallop <doyouseeitmyway@gmail.com>
2018-05-19 15:51:03 -0400

    Create layout to display a song as a list item. Add custom ArrayAdapter for song.

---
+++ app/src/main/java/com/example/android/electronmusicplayer/SongAdapter.java
@@ -0,0 +1,43 @@
+package com.example.android.electronmusicplayer;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class SongAdapter extends ArrayAdapter<Music> {
+
+    SongAdapter(Activity context, ArrayList<Music> list){
+        super(context, 0, list);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View songListItem = convertView;
+
+        if (songListItem == null) {
+            songListItem = LayoutInflater.from(getContext()).inflate(R.layout.song_item, parent, false);
+        }
+
+        Music currentMusic = getItem(position);
+
+        ImageView songArt = songListItem.findViewById(R.id.songArt);
+        songArt.setImageResource(currentMusic.getImageId());
+
+        TextView songTitle = songListItem.findViewById(R.id.songTitle);
+        songTitle.setText(currentMusic.getMusicTitle());
+
+        TextView songAlbum = songListItem.findViewById(R.id.songAlbum);
+        songAlbum.setText(currentMusic.getMusicDescription());
+
+        return songListItem;
+    }
+}
